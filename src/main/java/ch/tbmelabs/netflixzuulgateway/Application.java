498a06e6652062b68ac54967af9e67df3dd6d3cb
bbortt <timon.borter@post.ch>
2018-11-06 16:52:44 +0100

    finally manage to work with keycloak.

--- src/main/java/ch/tbmelabs/netflixzuulgateway/Application.java
+++ src/main/java/ch/tbmelabs/netflixzuulgateway/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableZuulProxy
+//@EnableZuulProxy
 @SpringCloudApplication
 public class Application {
 
--- src/main/java/ch/tbmelabs/netflixzuulgateway/configuration/KeycloakConfigResolverConfiguration.java
+++ src/main/java/ch/tbmelabs/netflixzuulgateway/configuration/KeycloakConfigResolverConfiguration.java
@@ -1,14 +1,37 @@
 package ch.tbmelabs.netflixzuulgateway.configuration;
 
-import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.KeycloakConfigResolver;
+import org.keycloak.adapters.KeycloakDeployment;
+import org.keycloak.adapters.KeycloakDeploymentBuilder;
+import org.keycloak.adapters.spi.HttpFacade.Request;
+import org.keycloak.representations.adapters.config.AdapterConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class KeycloakConfigResolverConfiguration {
 
+  private AdapterConfig adapterConfig;
+
+  public KeycloakConfigResolverConfiguration(AdapterConfig adapterConfig) {
+    this.adapterConfig = adapterConfig;
+  }
+
   @Bean
-  public KeycloakSpringBootConfigResolver keycloakConfigResolver() {
-    return new KeycloakSpringBootConfigResolver();
+  public KeycloakConfigResolver keycloakConfigResolver() {
+    return new CustomKeycloakSpringBootConfigResolver(adapterConfig);
+  }
+
+  public static class CustomKeycloakSpringBootConfigResolver implements KeycloakConfigResolver {
+
+    private KeycloakDeployment keycloakDeployment;
+
+    public CustomKeycloakSpringBootConfigResolver(AdapterConfig adapterConfig) {
+      this.keycloakDeployment = KeycloakDeploymentBuilder.build(adapterConfig);
+    }
+
+    public KeycloakDeployment resolve(Request facade) {
+      return keycloakDeployment;
+    }
   }
 }
